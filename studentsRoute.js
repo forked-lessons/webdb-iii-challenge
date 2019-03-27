7fc43305ff5a2eb7cc7c87cfdb3615191c6aa1c3
Daniel <danielstarling4@gmail.com>
2019-03-27 15:27:57 -0700

    students delete working

--- studentsRoute.js
+++ studentsRoute.js
@@ -44,4 +44,39 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+  try {
+    const student = await db('students')
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const student = await db('students')
+        .where({ id: req.params.id })
+        .first();
+
+      res.status(200).json(role);
+    } else res.status(200).json(student);
+  } catch (error) {
+    res.status(500).json(console.log(error));
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const count = await db('students')
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: 'Student not found' });
+    }
+    res.status(200).json(cohort);
+  } catch (error) {
+    res.status(500).json(console.log(error));
+  }
+});
+
 module.exports = router;
