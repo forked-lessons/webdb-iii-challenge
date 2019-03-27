1324f8d9d356f48a61225bf0b82b4d71718a09b3
Daniel <danielstarling4@gmail.com>
2019-03-27 12:12:33 -0700

    express api connecting

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+
+// const db = knex(knexConfig);
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+  try {
+    res.status(200).json('Hello World');
+  } catch {}
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () =>
+  console.log(`\n** API running on http://localhost:${port} **\n`)
+);
